cbc74d946e64d178a36f6915a27d396137d772f8
andromedagalaxia <brizuelautl@gmail.com>
2021-04-27 20:21:17 -0500

    Proyecto splashIcons

---
+++ Proyectos/Splashicono/Splashicono/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Splashicono
+//
+//  Created by Ma Guadalupe Brizuela on 27/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
