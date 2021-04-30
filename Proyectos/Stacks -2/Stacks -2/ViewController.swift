046dda42c5650f1b9b654fc196edb418bf196cc8
andromedagalaxia <brizuelautl@gmail.com>
2021-04-30 02:12:18 -0500

    Stacks view 2

---
+++ Proyectos/Stacks -2/Stacks -2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stacks -2
+//
+//  Created by Ma Guadalupe Brizuela on 30/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
