8b195dd9b22ad297879d4e77658daf20ecc369ea
andromedagalaxia <brizuelautl@gmail.com>
2021-04-29 19:57:50 -0500

    Hola mundo con breakpoints

---
+++ Proyectos/HolaMundo/HolaMundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HolaMundo
+//
+//  Created by Ma Guadalupe Brizuela on 29/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let cadena:String = "Desarrollo en Ios"
+        
+        print("Este es un mensaje antes del breakpoint")
+        
+        print("Este es un mensaje posterior al breakpoint")
+        print(cadena)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
