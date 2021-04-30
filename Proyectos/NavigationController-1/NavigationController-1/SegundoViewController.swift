6ceedb9fb20cd3fdab9d11f92e978d7f3338df3a
andromedagalaxia <brizuelautl@gmail.com>
2021-04-30 15:21:15 -0500

    viewcontroller

---
+++ Proyectos/NavigationController-1/NavigationController-1/SegundoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SegundoViewController.swift
+//  NavigationController-1
+//
+//  Created by Ma Guadalupe Brizuela on 30/04/21.
+//
+
+import UIKit
+
+class SegundoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Segundo View Controller"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Proyectos/NavigationController-1/NavigationController-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationController-1
+//
+//  Created by Ma Guadalupe Brizuela on 30/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
