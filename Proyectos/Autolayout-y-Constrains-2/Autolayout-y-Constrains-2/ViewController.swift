d082fdfbf1bbfc76cae11e31fc1936a047664033
andromedagalaxia <brizuelautl@gmail.com>
2021-04-29 21:55:31 -0500

    Constrains 2

---
+++ Proyectos/Autolayout-y-Constrains-2/Autolayout-y-Constrains-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autolayout-y-Constrains-2
+//
+//  Created by Ma Guadalupe Brizuela on 29/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
