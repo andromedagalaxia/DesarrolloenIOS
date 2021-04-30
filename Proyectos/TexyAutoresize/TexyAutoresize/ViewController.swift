731b2b003664fe313c1ccefdbe8a8597203f0428
andromedagalaxia <brizuelautl@gmail.com>
2021-04-30 02:25:17 -0500

    resize

---
+++ Proyectos/TexyAutoresize/TexyAutoresize/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TexyAutoresize
+//
+//  Created by Ma Guadalupe Brizuela on 30/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var NombreEmpleado: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        NombreEmpleado.text = "Ma Guadalupe Brizuela Gamiño"
+        
+    }
+
+
+}
+
