f977658940515d294fcb4bac6c3ac171f834427b
andromedagalaxia <brizuelautl@gmail.com>
2021-05-01 21:56:18 -0500

    imagenes usando view collection

---
+++ Proyectos/galeriaCollectionView/galeriaCollectionView/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  galeriaCollectionView
+//
+//  Created by Ma Guadalupe Brizuela on 01/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout  {
+
+    
+
+    @IBOutlet weak var fotos: UICollectionView!
+    
+    
+    
+    let imagenes: [String] = ["1","2","3","4","5"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fotos.delegate = self
+        fotos.dataSource = self
+        
+        
+    }
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return imagenes.count
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            
+            let celda = collectionView.dequeueReusableCell(withReuseIdentifier: "celda", for: indexPath) as! fotosCollectionViewCell
+            celda.foto.image = UIImage(named: imagenes[indexPath.row])
+                return celda
+                }
+            
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let pantalla = UIScreen.main.bounds
+        return CGSize(width: pantalla.width, height: pantalla.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    }
+
+
+
+
---
+++ Proyectos/galeriaCollectionView/galeriaCollectionView/fotosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  fotosCollectionViewCell.swift
+//  galeriaCollectionView
+//
+//  Created by Ma Guadalupe Brizuela on 01/05/21.
+//
+
+import UIKit
+
+class fotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var foto: UIImageView!
+    
+}
